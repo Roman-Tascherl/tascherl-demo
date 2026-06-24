bb158eb3c5b3c8c79ab3b3c6f00ede1f03fa3e6a
Roman-Tascherl <rrafetseder@student.tgm.ac.at>
2026-06-24 09:55:10 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ const initialCards = [
     type: "QR",
     category: "Online",
     color: "from-slate-400 to-black",
-    info: "Test Du Hund",
+    info: "Verwende es in einem Online Shop",
     code: "AMZ-22341",
     locationHint: "Online verwenden",
     favorite: true,
